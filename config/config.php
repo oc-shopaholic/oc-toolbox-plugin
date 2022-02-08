98b00890b944f74709020f875de177dbf95f0ff3
Igor Tverdokhleb <igor-tv@users.noreply.github.com>
2022-02-08 11:28:56 +0300

    Add plugin config

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | GraphQL Endpoint
+    |--------------------------------------------------------------------------
+    |
+    | Set the endpoint to which the GraphQL server responds.
+    | The default route endpoint is "yourdomain.com/graphql".
+    |
+     */
+
+    'route_name' => 'graphql',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Schema Declaration
+    |--------------------------------------------------------------------------
+    |
+    | This is a path that points to where your GraphQL schema is located
+    | relative to the app path. You should define your entire GraphQL
+    | schema in this file (additional files may be imported).
+    |
+     */
+
+//    'schema' => [
+//        'register' => plugins_path('lovata/toolbox/classes/api/schema.graphql'),
+//    ],
+
+
+];
