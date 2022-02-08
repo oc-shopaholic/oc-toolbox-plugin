02d97d608926b498582352e64f47244f94ca2823
Igor Tverdokhleb <igor-tv@users.noreply.github.com>
2022-02-08 19:02:44 +0300

    Add arguments for ProductCollectionType methods

--- classes/api/collection/ProductCollectionType.php
+++ classes/api/collection/ProductCollectionType.php
@@ -2,6 +2,7 @@
 
 use Lovata\Toolbox\Classes\Api\Item\ProductItemType;
 use Lovata\Toolbox\Classes\Api\Type\TypeFactory;
+use Lovata\Toolbox\Classes\Api\Type\Custom\Type as CustomType;
 
 use Lovata\Shopaholic\Classes\Collection\ProductCollection;
 use GraphQL\Type\Definition\Type;
@@ -40,7 +41,22 @@ class ProductCollectionType extends AbstractCollectionType
     protected function getArguments(): ?array
     {
         $arArgumentList = parent::getArguments();
+        $arArgumentList['brand'] = Type::int();
+        $arArgumentList['campaign'] = Type::int();
+        $arArgumentList['category'] = CustomType::array(); // TODO: add $bWithChildren param
+        $arArgumentList['couponGroup'] = Type::int();
+        $arArgumentList['discount'] = Type::int();
+        $arArgumentList['filterByBrandList'] = CustomType::array();
+//        $arArgumentList['filterByPrice'] = CustomType::array(); // TODO: ($fStartPrice, $fStopPrice, [$iPriceTypeID = null])
+//        $arArgumentList['filterByProperty'] = CustomType::array(); // TODO: ($arFilterList, $obPropertyList, [$obOfferList = null])
+        $arArgumentList['getOfferMaxPrice'] = Type::string();
+        $arArgumentList['getOfferMinPrice'] = Type::string();
+        $arArgumentList['label'] = Type::int();
+        $arArgumentList['promo'] = Type::int();
+        $arArgumentList['promoBlock'] = Type::int();
+        $arArgumentList['search'] = Type::string();
         $arArgumentList['sort'] = Type::string();
+        $arArgumentList['tag'] = Type::int();
 
         return $arArgumentList;
     }
