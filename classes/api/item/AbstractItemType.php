b2ece2a15f3378252572bf857ad163a05d92bec7
Igor Tverdokhleb <igor-tv@users.noreply.github.com>
2022-02-08 11:45:00 +0300

    Extend AbstractItemType, fix namespaces

--- classes/api/item/AbstractItemType.php
+++ classes/api/item/AbstractItemType.php
@@ -1,12 +1,13 @@
 <?php namespace Lovata\Toolbox\Classes\Api\Item;
 
+use Event;
+
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 
-use Lovata\Toolbox\Classes\Api\Type\AbstractApiType;
-use Lovata\Toolbox\Classes\Api\Type\TypeFactory;
+use Illuminate\Support\Arr;
 
-use Arr;
+use Lovata\Toolbox\Classes\Api\Type\AbstractApiType;
 
 /**
  * Class AbstractItemType
@@ -16,6 +17,7 @@ use Arr;
 abstract class AbstractItemType extends AbstractApiType
 {
     const ITEM_CLASS = '';
+    const EVENT_EXTEND_TYPE_FIELDS = '';
 
     /**
      * Get resolve method for type
@@ -38,7 +40,7 @@ abstract class AbstractItemType extends AbstractApiType
     /**
      * Returns element item by ID
      * @param int|string $iElementID
-     * @return \App\Classes\Item\AbstractElementItem
+     * @return \Lovata\Toolbox\Classes\Item\ElementItem
      */
     protected function findElement($iElementID)
     {
@@ -47,7 +49,7 @@ abstract class AbstractItemType extends AbstractApiType
         }
 
         $sItemClass = static::ITEM_CLASS;
-        /** @var \App\Classes\Item\AbstractElementItem $obItem */
+        /** @var \Lovata\Toolbox\Classes\Item\ElementItem $obItem */
         $obItem = $sItemClass::make($iElementID);
 
         return $obItem;
@@ -64,5 +66,74 @@ abstract class AbstractItemType extends AbstractApiType
         ];
 
         return $arArgumentList;
+    }
+
+    /**
+     * Get element item image
+     * @param $obItem
+     * @param $sFieldName
+     * @return array|null
+     */
+    protected function getImage($obItem, $sFieldName): ?array
+    {
+        $obImage = $obItem->{$sFieldName};
+        if (empty($obImage)) {
+            return null;
+        }
+
+        return $this->getImageData($obImage);
+    }
+
+    /**
+     * Get element item image list
+     * @param $obItem
+     * @param $sFieldName
+     * @return array|null
+     */
+    protected function getImageList($obItem, $sFieldName): ?array
+    {
+        $obImageList = $obItem->{$sFieldName};
+
+        if (empty($obImageList)) {
+            return null;
+        }
+
+        $arImageList = [];
+
+        foreach ($obImageList as $obImage) {
+            $arImageList[] = $this->getImageData($obImage);
+        }
+
+        return $arImageList;
+    }
+
+    protected function getImageData($obImage): array
+    {
+        return [
+            'url' => $obImage->getPath(),
+            'attributes' => $obImage->attributes
+        ];
+    }
+
+    /**
+     * Extend api item type field list
+     * @param $arFieldList
+     * @return array
+     */
+    protected function extendFieldList($arFieldList): array
+    {
+        if (empty(static::EVENT_EXTEND_TYPE_FIELDS)) {
+            return $arFieldList;
+        }
+
+        $arEventFieldList = Event::fire(static::EVENT_EXTEND_TYPE_FIELDS, [static::instance()]);
+
+        if (!empty($arEventFieldList)) {
+            foreach ($arEventFieldList as $arEventFields) {
+                $arFieldList = array_merge($arFieldList, $arEventFields);
+            }
+        }
+
+        return $arFieldList;
     }
 }
