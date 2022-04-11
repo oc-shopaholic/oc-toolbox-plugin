de8854be1668123bd21a56ba16d98206483519ea
Igor Tverdokhleb <i.tverdokhleb@lovata.com>
2022-04-12 01:53:24 +0300

    Refactor routes and ApiController

--- http/controllers/ApiController.php
+++ http/controllers/ApiController.php
@@ -1,8 +1,10 @@
 <?php namespace Lovata\Toolbox\Http\Controllers;
 
 use App;
-use Log;
+use Backend\Classes\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Log;
 
 use Lovata\Toolbox\Classes\Api\Response\ApiDataResponse;
 use Lovata\Toolbox\Classes\Api\Type\FrontendTypeFactory;
@@ -11,60 +13,39 @@ use Lovata\Toolbox\Classes\Api\QueryProcessor;
 /**
  * Class ApiController
  * @package Lovata\Toolbox\Http\Controllers
+ * @return \Illuminate\Http\JsonResponse
  */
-class ApiController
+class ApiController extends Controller
 {
-    protected $obRequest;
-    protected $obApiResponse;
-
-    public function __construct($obRequest)
+    public function __invoke(Request $obRequest)
     {
-        $this->obRequest = $obRequest;
-        $this->obApiResponse = ApiDataResponse::instance();
-    }
-
-    /**
-     * Execute api request and return response as json string
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function query()
-    {
-        $sQuery = $this->obRequest->get('query');
-        $arVariables = $this->obRequest->get('variables');
+        $sQuery = $obRequest->get('query');
+        $arVariables = $obRequest->get('variables');
 
         //Execute query and get result object
         $obQueryProcessor = new QueryProcessor($sQuery, $arVariables, FrontendTypeFactory::class);
-        $this->executeQuery($obQueryProcessor);
-
-        return $this->obApiResponse->response();
-    }
-
-    /**
-     * Execute query processor
-     * @param QueryProcessor $obQueryProcessor
-     */
-    protected function executeQuery($obQueryProcessor)
-    {
         $obResult = $obQueryProcessor->execute();
         $arResult = $obResult->toArray();
 
         if (!empty($obResult->errors)) {
-            $arErrorList = $obResult->errors;
-            $obFirstError = array_shift($arErrorList);
-            $this->obApiResponse->setErrorMessage(
+            $sMessage = App::isProduction() ? Arr::get($arResult, 'errors.0.message') : $obResult->errors[0]->getMessage();
+            $arErrors = App::isProduction() ? $obResult->toArray() : $obResult->errors;
+            ApiDataResponse::instance()->setErrorMessage(
                 ApiDataResponse::CODE_ERROR,
-                App::environment('production') ? Arr::get($arResult, 'errors.0.message') : $obFirstError->getMessage(),
-                App::environment('production') ? $obResult->toArray() : $obResult->errors
+                $sMessage,
+                $arErrors
             );
 
-            $obPrevious = $obFirstError->getPrevious();
+            $obPrevious = $obResult->errors[0]->getPrevious();
             Log::error('Query: '.$obQueryProcessor->getQuery());
-            Log::error('Error: '.$obFirstError->getMessage());
+            Log::error('Error: '.$obResult->errors[0]->getMessage());
             if (!empty($obPrevious)) {
-                Log::error('Line: '.$obPrevious->getLine().', File: '.$obPrevious->getFile().', Message: '.$obPrevious->getMessage());
+                Log::error(sprintf('Line: %d, File: %s, Message: %s', $obPrevious->getLine(), $obPrevious->getFile(), $obPrevious->getMessage()));
             }
         } else {
-            $this->obApiResponse->setData(Arr::get($arResult, 'data'));
+            ApiDataResponse::instance()->setData(Arr::get($arResult, 'data'));
         }
+
+        return ApiDataResponse::instance()->response();
     }
 }
--- routes.php
+++ routes.php
@@ -1,11 +1,3 @@
 <?php
 
-use Lovata\Toolbox\Http\Controllers\ApiController;
-
-Route::post(config('lovata.toolbox::route_name'), function () {
-    $obApiController = new ApiController(Request::instance());
-    $sResponse = $obApiController->query();
-
-    return $sResponse;
-
-})->middleware('api');
+Route::post(config('lovata.toolbox::route_name'), Lovata\Toolbox\Http\Controllers\ApiController::class)->middleware('api');
