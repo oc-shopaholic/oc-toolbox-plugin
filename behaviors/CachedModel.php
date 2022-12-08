711ee7cf12eff7a5bb8e5069854c973a4390407d
Igor Tverdokhleb <i.tverdokhleb@lovata.com>
2022-12-08 17:47:30 +0300

    Fix namespace

--- behaviors/CachedModel.php
+++ behaviors/CachedModel.php
@@ -1,11 +1,11 @@
-<?php namespace Lovata\Toolbox\Classes\Behavior;
+<?php namespace Lovata\Toolbox\Behaviors;
 
 use October\Rain\Extension\ExtensionBase;
 use Lovata\Toolbox\Traits\Helpers\TraitCached;
 
 /**
  * Class CachedModel
- * @package Lovata\Toolbox\Classes\Behavior
+ * @package Lovata\Toolbox\Behaviors
  * @author  Igor Tverdokhleb, i.tverdokhleb@lovata.com, LOVATA Group
  */
 class CachedModel extends ExtensionBase
