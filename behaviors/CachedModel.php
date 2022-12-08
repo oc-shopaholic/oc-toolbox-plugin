637c80f4ea34112cd617f610c7037c101c517ab1
Igor Tverdokhleb <i.tverdokhleb@lovata.com>
2022-12-08 17:21:03 +0300

    Add CachedModel behavior

---
+++ behaviors/CachedModel.php
@@ -0,0 +1,14 @@
+<?php namespace Lovata\Toolbox\Classes\Behavior;
+
+use October\Rain\Extension\ExtensionBase;
+use Lovata\Toolbox\Traits\Helpers\TraitCached;
+
+/**
+ * Class CachedModel
+ * @package Lovata\Toolbox\Classes\Behavior
+ * @author  Igor Tverdokhleb, i.tverdokhleb@lovata.com, LOVATA Group
+ */
+class CachedModel extends ExtensionBase
+{
+    use TraitCached;
+}
